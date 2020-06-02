4e4b49f40bad525dd71abe45723a2ebb7282d54b
VictoriaA13 <65688626+VictoriaA13@users.noreply.github.com>
2020-06-02 20:57:35 +0100

    Add initial CV website files

---
+++ script.js
@@ -0,0 +1,9 @@
+website = "https://arkmont.com/";
+"new_string = "The quick brown fox jumps of the lazy dog"
+new_string = new_string.toLowerCase()
+url = new_string.replace (" ", " ")
+url - url.split (" ")
+url
+website + url"https://arkmont.com/the.quick,brown,fox,jumps,of,the,lazy,dog"
+url = url.join(" ")
+url = website + url
